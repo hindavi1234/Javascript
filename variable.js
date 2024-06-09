8f6974acc1c492a1896f669706bd14ec7226503d
Hindavi Sanjay Patil <106336510+hindavi1234@users.noreply.github.com>
2024-06-09 15:55:54 +0000

    confing done

---
+++ variable.js
@@ -0,0 +1,19 @@
+const account_Id = 1433;
+let account_email = "hindaviptl4u@gmail.com"
+var account_password = "12434";
+account_city = "jaipur";
+let account_state;
+
+// account_Id = 23;
+
+console.log(account_Id);
+
+/*
+   Prefer not to use var
+   because of issue in block scope and functional scope
+*/
+account_email = "hcdahg@gmail.com"
+account_password = "2342q35"
+account_city = "Bengluru"
+
+console.table([account_Id, account_email, account_password, account_city, account_state])
