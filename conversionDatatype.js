c605544c356ae38be9bc9f2493c5fcc6c94e0f8b
Hindavi Sanjay Patil <106336510+hindavi1234@users.noreply.github.com>
2024-06-11 05:10:24 +0000

    conversion

---
+++ conversionDatatype.js
@@ -0,0 +1,24 @@
+let score = true;
+
+// console.log(typeof name);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber))
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+/*
+  "33" => 33
+  "33absc" => NAN 
+  true => 1 
+  false => 0
+
+  */ 
+
+  let someNumber = 33;
+  let stringNumber = String(someNumber);
+  console.log(typeof stringNumber)
