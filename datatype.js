fdb35cd8fd734ff5c85151889859e6a972cdb1d5
Hindavi Sanjay Patil <106336510+hindavi1234@users.noreply.github.com>
2024-06-11 04:50:49 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,21 @@
+let name = "HIndaib";
+let age = 23;
+let isLogged = false;
+
+/*
+   number => 2 to powerr 53
+   bigint
+   string => ""
+   boolean => true/false
+   null => standalone value
+   undefined => not assinged any value ye
+   symbol => to find uniqueness
+
+   object
+*/ 
+
+console.log(typeof "hindavi");
+console.log(typeof age);
+console.log(typeof isLogged);
+console.log(typeof null);//object
+console.log(typeof undefined); //undefined
